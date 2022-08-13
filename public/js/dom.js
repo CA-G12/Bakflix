986ec17975da348522a72122b4772549372857cb
Bas-sheikh <basilelshakhe@gmail.com>
2022-08-13 17:44:36 +0300

    added more details function thats well show more details about series. relates #41

--- public/js/dom.js
+++ public/js/dom.js
@@ -4,6 +4,7 @@ const btnLandingSection = document.querySelector('#btn');
 const movieSuggestionsImages = document.querySelector('.movies-suggestions-images');
 const movieSuggestionsNames = document.querySelector('.movies-suggestions-names');
 const searchInput = document.querySelector('input');
+const searchBox = document.querySelector(`.search`);
 
 const input = document.querySelector('#input');
 
@@ -19,6 +20,7 @@ window.onload = () => {
 btnLandingSection.addEventListener('click', () => {
   landingSection.style.display = 'none';
   browseSection.style.display = 'block';
+  searchBox.style.visibility = `visible`;
 });
 
 // for git data from json by using fetch api function
@@ -48,12 +50,17 @@ function removeDuplicates(mainDiv) {
 function handleDom(data) {
   removeDuplicates(movieSuggestionsNames)
   removeDuplicates(movieSuggestionsImages)
-  for (let i = 0; i < data.length; i++) {
+  for (let i = 0; i < 10; i++) {
     const names = data[i][`name`];
     const img = data[i][`img`];
     if (names.toLowerCase().includes(input.value.toLowerCase())) {
       console.log(input.value.split(' ').join(''));
       const movieName = document.createElement('a')
+      movieName.setAttribute(`id`, data[i][`id`])
+      movieName.addEventListener(`click`, (e) => {
+        const id = data[i][`id`]
+        fetch(`https://api.tvmaze.com/shows/${id}`, getDetails)
+      })
       movieName.textContent = names
       movieSuggestionsNames.appendChild(movieName)
       const movieImage = document.createElement('img')
@@ -61,4 +68,28 @@ function handleDom(data) {
       movieSuggestionsImages.appendChild(movieImage)
     }
   }
+}
+
+// show details function
+function getDetails (data) {
+  movieSuggestionsImages.style.overflow = `hidden`
+  movieSuggestionsImages.textContent = ``;
+  let div = document.createElement(`div`);
+  div.classList.add(`result`)
+  let imgDiv = document.createElement(`div`);
+  let img = document.createElement(`img`);
+  let container = document.createElement(`div`);
+  let divPara = document.createElement(`div`);
+  let heading = document.createElement(`h1`);
+
+  img.src = data.image.medium;
+  divPara.innerHTML = data.summary;
+  heading.textContent = data.name;
+  
+  imgDiv.appendChild(img)
+  div.appendChild(imgDiv);
+  div.appendChild(container);
+  container.appendChild(heading);
+  container.appendChild(divPara);
+  movieSuggestionsImages.appendChild(div)
 }
